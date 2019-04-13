8c29984fb2f452ca27e5f5c1aec49a361939feb3
Philipe Sena Saviott <psaviott@hotmail.com>
2019-04-13 17:11:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Logs Analysis
+
+
+## Project Overview
+
+This project work with data that could have come from a real-world web application, with fields representing information that a web server would record, such as HTTP status codes and URL paths. The web server and the reporting tool both connect to the same database, allowing information to flow from the web server into the report.
+Exploring a large database with over a million rows we building and refine complex queries and use them to draw business conclusions from data.
+
+## Instructions
+This project use a webserver with PostgreSQL and Python3. You can run this in a real server or in a virtual machine. If you need help to install and setup a virtual machine with VirtualBox and Vagrant, check the reference link bellow on Resources.
+
+### Requirements
+* [Python3](https://www.python.org/ "Python Homepage")
+* [PostgreSQL](https://www.postgresql.org/ "PostgreSQL Homepage")
+* [Web Server](https://en.wikipedia.org/wiki/Web_server/ "Wikipedia article about Web Servers")
+
+### How-to-Run
+* Clone this git repositoriy:
+    ```
+      $ git clone https://github.com/psaviott/Logs-Analysis.git'
+    ```
+* [Download](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip "download newsdata.sql") the newsdata file.
+
+* Setup database
+
+  * Create a database named news
+    ```psql
+      user=> CREATE DATABASE news;
+    ```
+  * Load newsdata.sql into local database:
+    ```
+      $ psql -d news -f newsdata.sql
+    ```
+* Run program with:
+  ```python
+    $ python3 log-analysis.py
+  ```
+
+## Resources
+* [PostgreSQL](https://www.postgresql.org/docs/10/index.html "PostgreSQL documentation") documentation
+* [Python3](https://docs.python.org/3.6/index.html "Python3 documentation") documentation
+* [VirtualBox and Vagrant](https://www.taniarascia.com/what-are-vagrant-and-virtualbox-and-how-do-i-use-them/ "How to use Vagrant and VirtualBox") by Tania Rascia
+
+## Credits
+  Philipe Sena Saviott
---
+++ log-analysis.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+
+import psycopg2
+
+
+# Reporting tool queries
+# Query 1: What are the three most popular articles of all time?
+query1 = """select title, count(path)
+            from articles
+            inner join log on ('/article/' || articles.slug like log.path)
+            group by articles.title
+            order by count desc
+            fetch first 3 row only;"""
+
+
+# Query 2: Who are the most popular article authors of all time?
+query2 = """select name, count(path)
+            from authors, log
+            inner join articles on ('/article/' || articles.slug like log.path)
+            where authors.id = articles.author
+            group by authors.name
+            order by count desc;"""
+
+# Query 3: On which day did more than 1% of requests lead to errors?
+query3 = """select date(time),
+                    cast((count(log.status)*100.0)/num as decimal(6,1)) as x
+            from log, (select date(time), count(status) as num
+                from log
+                group by date(time)) as perc
+            where date(time) = date
+            and log.status = '404 NOT FOUND'
+            group by date(time), num
+            having count(log.status) > num/100
+            order by x desc;"""
+
+
+# Open and close the connection
+def do_query(query):
+    data = psycopg2.connect(database="news")
+    consult = data.cursor()
+    consult.execute(query)
+    qresult = consult.fetchall()
+    data.close()
+    return qresult
+
+
+# Print report
+def print_querys():
+
+    # Print top articles
+    articles = do_query(query1)
+    print("\n\t Top articles of all time \n")
+    for title, count in articles:
+        print(title, "-", count, "views")
+
+    # Print top authors
+    authors = do_query(query2)
+    print("\n\t Top authors of all time \n")
+    for name, count in authors:
+        print(name, "-", count, "views")
+
+    # Print high error day
+    errors = do_query(query3)
+    print("\n\t Days with more than ""1%"" of bad requests \n")
+    for date, x in errors:
+        print(date, "-", x, """% errors""" "\n")
+
+
+if __name__ == '__main__':
+    print_querys()
